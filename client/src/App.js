05fa2054f9c9ec892f43b87398340f3427970dcc
Dave Mastropolo <davemastropolo@Margarets-MacBook-Air.local>
2023-04-17 16:49:26 -0700

    App.js development change to title for testing

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Fib calculator
+            Development br Learn React Fib calculator
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
