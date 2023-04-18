3ba8ffea42c1df05e4fbd387fe23946ccc3e883f
Dave Mastropolo <davemastropolo@Margarets-MacBook-Air.local>
2023-04-17 18:59:39 -0700

    App.js change

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            2nd dev Development br Learn React Fib calculator
+            3rd 2nd dev Development br Learn React Fib calculator
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
