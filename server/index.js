19b194a6bed4ceca62104bf3aaacad5ebc420b0f
Dave Mastropolo <davemastropolo@Margarets-MacBook-Air.local>
2023-04-27 12:58:37 -0700

    client App.js and sever index.js changes

--- server/index.js
+++ server/index.js
@@ -41,9 +41,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/values/all", async (req, res) => {
-  const values = await pgClient.query("SELECT * from values");
-
-  res.send(values.rows);
+  //const values = await pgClient.query("SELECT * from values");
+  res.send ([1, 2, 3]);
+  //res.send(values.rows);
 });
 
 app.get("/values/current", async (req, res) => {
